b5d6575a05aad5ff2ed843fdf56276b9055c0f44
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-11-24 21:14:02 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 cd C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\
-MSBuild.exe
-C:\ProgramData\Jenkins\.jenkins\workspace\Test-job
+MSBuild.exe C:\ProgramData\Jenkins\.jenkins\workspace\Test-job\Test
