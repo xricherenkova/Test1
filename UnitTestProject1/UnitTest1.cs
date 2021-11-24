ab86f4b5db3e20c48c2d71d85060b33c4020a298
Xristina <Xristina@CHERENKOVA>
2021-11-24 18:54:52 +0400

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Test;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Animal animal = new Animal();
+            int expectedNumber = 5;
+            int number = animal.GetNumberOfLegs(expectedNumber);
+            Assert.AreEqual(expectedNumber, number);
+        }
+    }
+}
