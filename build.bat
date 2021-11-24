f5f2f935b27c9c9315c3518ff44c18ce271795f4
xricherenkova <79928904+xricherenkova@users.noreply.github.com>
2021-11-24 20:45:15 +0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-C:\ProgramFiles(x86)\MicrosoftVisualStudio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe
+C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe
